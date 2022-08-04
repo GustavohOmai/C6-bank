7f6ba8005f25e7df8da64617c506978d8ce76f76
Breno Tosi <breno_tosi@hotmail.com>
2022-08-03 21:49:48 -0300

    Header JS | Experience Atualizado

--- c6-experience.js
+++ c6-experience.js
@@ -1,4 +1,20 @@
 window.onload = function() {
-  
+  window.onscroll = function() {
+        scroll();
+    }
+
+    function scroll() {
+        if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+            document.getElementById("btn").style.background ="rgb(255, 193, 7)";
+            document.getElementById("btn").style.border ="none";
+            document.getElementById("btn").style.color ="rgb(0,0,0)";
+    
+        }
+        else {
+            document.getElementById("btn").style.background= "rgb(25, 25, 25)";
+            document.getElementById("btn").style.border ="white 1px solid";
+            document.getElementById("btn").style.color ="rgb(255,255,255)";
+        }
+    }
   
 }
